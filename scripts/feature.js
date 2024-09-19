2c669346d70bac2ccb75ef59691efa505f04807f
Litonuddinnil <mdniloyhasan544@gmail.com>
2024-09-19 17:52:03 +0600

    complete comercial website

--- scripts/feature.js
+++ scripts/feature.js
@@ -1,1 +1,9 @@
-console.log('i am add')
+document.getElementById('btn-show-cashOut').addEventListener('click',function(){
+    document.getElementById('cash-out-from').classList.remove('hidden');
+   document.getElementById('add-money-from').classList.add('hidden');
+
+})
+document.getElementById('btn-add-money').addEventListener('click',function(){
+    document.getElementById('cash-out-from').classList.add('hidden');
+    document.getElementById('add-money-from').classList.remove('hidden');
+})
